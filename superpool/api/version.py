76cbd4d01fcca81c13d97f7e35db0e2f59301a06
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-06-06 07:47:08 +0100

    chore: add packaging versioning

---
+++ superpool/api/version.py
@@ -0,0 +1,5 @@
+# This file is part of the package versioning
+#
+# Contains the version of the package that is automatically inserted into the
+# API documentation and the package itself.
+VERSION = "0.1.0"
