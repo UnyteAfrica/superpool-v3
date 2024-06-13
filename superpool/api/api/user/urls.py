c4561d4b720e03d1cfe24080015fb79b64bbb7e4
50-Course <eridotdev@gmail.com>
2024-06-13 08:16:36 +0100

    setup auth endpoints

--- superpool/api/api/user/urls.py
+++ superpool/api/api/user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import SignInView, SignUpView
 
 urlpatterns = [
-    path("auth/register/", SignUpView.as_view(), name="signup"),
-    path("auth/signin/", SignInView.as_view(), name="signin"),
+    path("register/", SignUpView.as_view(), name="signup"),
+    path("signin/", SignInView.as_view(), name="signin"),
 ]
