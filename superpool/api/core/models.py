11bc3b3ec5f48dc319e8e3710536e45f963a6bb8
50-Course <eridotdev@gmail.com>
2024-06-17 06:24:33 +0100

    WIP(Policies): add Insurance policy

--- superpool/api/core/models.py
+++ superpool/api/core/models.py
@@ -1,3 +1,4 @@
+from api.core.mixins import TimestampMixin
 from core.user.models import User  # noqa: F401
 from django.db import models
 
@@ -25,3 +26,45 @@ class Address(models.Model):
 
     def __str__(self) -> str:
         return f"<Address: {self.street}, {self.city}, {self.state}, {self.country}>"
+
+
+class Policy(TimestampMixin, models.Model):
+    """
+    Represent an insurance policy
+
+    """
+
+    class Status(models.TextChoices):
+        ACTIVE = "Active"
+        CANCELLED = "Cancelled"
+        EXPIRED = "Expired"
+
+    policy_id: models.AutoField = models.AutoField(primary_key=True)
+    policy_name: models.CharField = models.CharField(
+        _("Policy Name"), max_length=100, help_text="Name of the insurance policy"
+    )
+    policy_number = models.CharField(max_length=100)
+    premium: models.DecimalField = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text="Final premimum amount after adjustments",
+    )
+
+    @property
+    def is_claimable(self) -> bool:
+        """
+        Check if the policy is claimable
+        """
+        return False
+
+    def __str__(self) -> str:
+        return f"Policy: {self.policy_name}"
+
+    @property
+    def is_renewable(self) -> bool:
+        return False
+
+    class Meta:
+        db_table = "policy"
+        verbose_name = "Policy"
+        verbose_name_plural = "Policies"
