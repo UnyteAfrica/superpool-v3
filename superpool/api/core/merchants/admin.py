3df6a12f5681d370d3f0144d5f2eb5e531f3119b
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-06-21 16:15:56 +0100

    feat(Merchants:Admin): Add `merchant` deactivation functionality in admin
    This patch introduces the ability to deactivate merchants from the Django admin interface.
    
    While, this is just a thought out-process, security guard such as
    permission-level checking would be added in future
    
    * A new custom method to "Deactivate Merchant" has been added to the Merchant admin.
    * Selecting this action and confirming will deactivate selected merchants by setting the `is_active` field to `False`.
    
    [ ] Unit tests has been added to confirm this functionality

--- superpool/api/core/merchants/admin.py
+++ superpool/api/core/merchants/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from rest_framework.request import Request
 
-# Register your models here.
+from .models import Merchant
+
+
+class MerchantModelAdmin(admin.ModelAdmin):
+    """
+    Admin model for Merchant model
+    """
+
+    list_display = ("name", "support_email", "is_active", "created_at", "updated_at")
+    search_fields = ("name", "short_code")
+    list_filter = ("created_at", "updated_at")
+    date_hierarchy = "created_at"
+    readonly_fields = ("created_at", "updated_at")
+    fieldsets = (
+        (
+            "Merchant Details",
+            {
+                "fields": (
+                    "name",
+                    "short_code",
+                    "support_email",
+                    "is_active",
+                )
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+
+    def deactivate_merchant(self, request: Request, queryset: QuerySet):
+        """
+        Deactivate selected merchants
+        """
+        queryset.update(is_active=False)
