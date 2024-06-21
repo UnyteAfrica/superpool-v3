8d4c252db864a15743078f5bee80cd618df04929
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-06-21 16:49:44 +0100

    WIP(Merchants API): setup boilerplate for creating a new merchant

---
+++ superpool/api/api/merchants/views.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ViewSet
+
+
+class MerchantViewset(ViewSet):
+    """
+    Viewset for Merchant model
+    """
+
+    def create(self, request: Request) -> Response:
+        """
+        Create a new Merchant
+        """
+        pass
