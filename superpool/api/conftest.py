79922e25290c89028f479e6fcb988be0e3ea605b
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-06-27 15:09:39 +0100

    refactor(conftest.py): inject `test_settings` into AppRegistry
    This commit allows us to specify a list of test-specifc setting
    configurations for django startup
    
    This is done through django's AppRegistry, using the `djano.setup()`
    method

--- superpool/api/conftest.py
+++ superpool/api/conftest.py
@@ -19,14 +19,14 @@ def configure_tests_db():
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": ":memory:",
     }
+    test_settings["DEBUG"] = True
+    test_settings["TEST_RUNNER"] = "django.test.runner.DiscoverRunner"
+    test_settings["DATABASE_ROUTERS"] = []
 
-    with override_settings(
-        DATABASES=test_settings["DATABASES"],
-    ):
-        settings.configure(
-            DEBUG=True,
-        )
+    with override_settings(**test_settings):
+        settings.configure(**test_settings)
 
+        # We need to reload the models to avoid the "AppRegistryNotReady: Apps aren't loaded yet." error
         import django
 
         django.setup()
