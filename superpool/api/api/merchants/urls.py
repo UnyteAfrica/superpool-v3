8388f25d7cf43ee7f2b9749233c90c9cc12f6cb4
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-01 11:20:17 +0100

    feat(merchants): update url configurations

--- superpool/api/api/merchants/urls.py
+++ superpool/api/api/merchants/urls.py
@@ -4,12 +4,12 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r"merchants", MerchantViewSet, basename="merchants")
+router.register(r"", MerchantViewSet, basename="merchants")
 
 
 urlpatterns = [
     path(
-        "merchants",
+        "",
         MerchantViewList.as_view({"get": "list"}),
         name="list_merchants",
     ),
