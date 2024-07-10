768125de95cd72611f8370e3a408f9c271990389
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-10 15:00:20 +0100

    Update Unyte's details on swagger

--- superpool/api/config/settings/contrib/swagger.py
+++ superpool/api/config/settings/contrib/swagger.py
@@ -1,6 +1,14 @@
+AUTHOR = "Unyte Africa LTD."
+
 # TODO: Dynamically insert the correct fields
 SPECTACULAR_SETTINGS = {
     "TITLE": "Superpool API",
     "DESCRIPTION": "API for Superpool",
     "VERSION": "0.1.0",
+    "AUTHOR": AUTHOR,
+    "CONTACT": {
+        "name": AUTHOR,
+        "url": "https://ng.unyte.africa",
+        "email": "tech@unyte.com",
+    },
 }
