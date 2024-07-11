9a3e11ff94db6cb3b9b6c16eaf84ae2fce1073f5
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-11 21:16:47 +0100

    feat: Track `X-APP-ID` in Custom Authentication Class

--- superpool/api/api/app_auth/authentication.py
+++ superpool/api/api/app_auth/authentication.py
@@ -1,6 +1,10 @@
-from core.models import Application
+import logging
+
+from core.models import APIKey, Application
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
+
+logger = logging.getLogger(__name__)
 
 
 class APIKeyAuthentication(BaseAuthentication):
@@ -12,21 +16,25 @@ class APIKeyAuthentication(BaseAuthentication):
     def authenticate(self, request):
         # Get both the X-APP-ID and API-KEY headers
         x_app_id = request.headers.get("X-APP-ID")
+        key = request.headers.get("API-KEY")
 
         # If X-APP-ID header is not present, return None
-        if not x_app_id:
+        if not x_app_id and not key:
             return None
 
         try:
             app = Application.objects.get(application_id=x_app_id)
+            apikey_obj = APIKey.objects.select_related("merchant").get(key=key)
 
         except Application.DoesNotExist:
             raise AuthenticationFailed("Invalid X-APP-ID")
+        except APIKey.DoesNotExist:
+            raise AuthenticationFailed("Invalid API Key")
 
-        request.application = app
-        request.merchant = app.merchant
+        if not (apikey_obj.merchant.short_code == app.merchant.short_code):
+            raise Exception("Merchant Objects are not the same")
 
-        return (app.merchant, None)
+        return (app.merchant, apikey_obj, None)
 
     def authenticate_header(self, request) -> str | None:
         return "X-APP-ID"
