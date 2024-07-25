78b1045c118697d0a82a609c4f8b3914fdc059cc
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-25 10:14:43 +0100

    perf: Implement memory-efficient unique ID generator

--- superpool/api/core/utils.py
+++ superpool/api/core/utils.py
@@ -1,3 +1,5 @@
+import random
+import string
 import uuid
 
 from core.merchants.models import Merchant
@@ -7,15 +9,10 @@ def generate_verification_token() -> str:
     """
     Generates a verification token for email verification
     """
-    import random
-    import string
-
-    return random.choice(
-        [
-            "".join(random.choices(string.ascii_letters + string.digits, k=1))
-            for _ in range(6)
-        ]
-    )
+    return random.choice([
+        "".join(random.choices(string.ascii_letters + string.digits, k=1))
+        for _ in range(6)
+    ])
 
 
 def send_verification_email(email: str, token: str) -> None:
@@ -38,9 +35,18 @@ def generate_id(klass):
     Performant ID generator that generates memory-efficient, unique idenifiers
     for objects
     """
-    class_name = klass
-    prefix = class_name[:3].lower().encode("utf-8")
-    suffix = uuid.uuid4().hex[:16].encode("utf-8")
-    # print(f"Prefix: {prefix}")
-    # print(f"Suffix: {suffix}")
-    return f"{prefix.decode()}_{suffix.decode()}"
+    # The logic here is to take a class name, append some random string values to it
+    # and str converted UUID suffix (the first block of UUIDs) - totalling upto 18 characters
+    #
+    # The length of the generated ID will be 17 characters (3 + 1 + 5 + 1 + 8)
+
+    class_name = klass.__name__
+    prefix = class_name[:3].lower()  # First 3 characters of the class name in lowercase
+
+    # random string of 5 characters
+    random_str = "".join(random.choices(string.ascii_lowercase + string.digits, k=5))
+
+    # first 8 characters of a UUID (this is actually 4 bytes)
+    uuid_suffix = uuid.uuid4().hex[:8]
+    unique_id = f"{prefix}_{random_str}_{uuid_suffix}"
+    return unique_id
