83901fbac061200265e8a5b4bebde3836b8e7bc6
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-25 11:17:57 +0100

    chore: Update create and update methods for quote serializer

--- superpool/api/api/catalog/serializers.py
+++ superpool/api/api/catalog/serializers.py
@@ -1,4 +1,4 @@
-from core.catalog.models import Policy, Product, Quote
+from core.catalog.models import Policy, Price, Product, Quote<
 from django.db.models import fields
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
@@ -65,10 +65,44 @@ class CreatePolicySerializer(ModelSerializer):
         exclude = ("cerfication_required", "inspection_required")
 
 
+class PriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Price
+        exclude = ["id"]
+
+
 class QuoteSerializer(serializers.ModelSerializer):
+    price = PriceSerializer(source='premium')
+    product = ProductSerializer()
+    quote_expiry_date=serializers.DateTimeField(source='expires_in', read_only=True)
+
     class Meta:
         model = Quote
-        fields = ("id", "base_price")
+        fields = ['quote_code', 'base_price', 'product', 'quote_expiry_date', 'price']
+        extra_kwargs={
+            'quote_code': {'read_only': True}
+        }
+
+    def create(self, validated_data):
+        # Verify that when creating new quote there is a quote_code, corresponding product,
+        # and calculated premium
+        product_data = validated_data.pop('product')
+        price_data = validated_data.pop('premium')
+
+        product = Product.objects.create(**product_data)
+        price = Price.objects.create(**price_data)
+
+        quote  = Quote.objects.create(product=product, premium=price, **validated_data)
+        return quote
+
+    def update(self, instance, validated_data):
+        # when updating quotes we only care about two thing, price and the quote_code
+        price_data = validated_data.pop('premium', None)
+
+        if price_data:
+            # Update the Price object with new data
+            Price.objects.filter(pk=instance.premium.pk).update(**price_data)
+        return super().update(instance, validated_data)
 
 
 class PolicyPurchaseSerializer(serializers.Serializer):
