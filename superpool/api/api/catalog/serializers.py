4a264dc0ceb9c70662cefc4a1f2eff2eb766ded2
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-26 20:36:24 +0100

    Implement serializers for policy porchase request

--- superpool/api/api/catalog/serializers.py
+++ superpool/api/api/catalog/serializers.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+from typing import Union
+
 from core.catalog.models import Policy, Price, Product, Quote
+from core.user.models import Customer
 from django.db.models import fields
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 
 class ProductSerializer(ModelSerializer):
@@ -103,18 +107,129 @@ class QuoteSerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
 
-class PolicyPurchaseSerializer(serializers.Serializer):
+class CustomerDetailsSerializer(serializers.Serializer):
     """
-    Issues a new policy based on CustomerDetails and some
-    other metadata
+    Validate customer-specific information in incoming requests
+
+    Information could vary based on the type of insurance
+
+    TODO: maybe use composition/inheritance for child serializers for
+          the product-specific customer information:
+
+     - AutoInsuranceDetailsSerializer
+     - HealthInsuranceDetailsSerializer
+     - PersonalAccidentInsuranceDetailsSerializer
+     - TravelInsuranceDetailsSerializer
     """
 
-    customer_details = serializers.JSONField()
-    category = serializers.CharField()
-    product_id = serializers.UUIDField()
-    coverage_id = serializers.UUIDField()
-    merchant_id = serializers.UUIDField()
-    provider_id = serializers.UUIDField()
-    renewable = serializers.BooleanField(required=False)
-    inspection_required = serializers.BooleanField(required=False)
-    certification_required = serializers.BooleanField(required=False)
+    first_name = serializers.CharField(max_length=100)
+    last_name = serializers.CharField(max_length=100)
+    customer_email = serializers.EmailField()
+    customer_phone = serializers.CharField(max_length=20, required=False)
+    customer_address = serializers.CharField()
+
+
+class QuoteRequestSerializer(serializers.Serializer):
+    """Validate the entire quote request payload"""
+
+    insurance_type = serializers.ChoiceField(
+        choices=[
+            ("health", "Health Insurance"),
+            ("auto", "Auto Insurance"),
+            ("travel", "Travel Insurance"),
+            ("personal_accident", "Personal Accident Insurance"),
+        ]
+    )
+    quote_code = serializers.CharField(required=False)
+    insurance_name = serializers.CharField(required=False)
+    customer_metadata = CustomerDetailsSerializer()
+
+
+class ProductMetadataSerializer(serializers.Serializer):
+    product_name = serializers.CharField(max_length=255)
+    product_type = serializers.CharField(max_length=50)
+    insurer = serializers.CharField(max_length=100, required=False)
+
+
+class PaymentInformationSerializer(serializers.Serializer):
+    payment_method = serializers.CharField(max_length=50)
+    payment_status = serializers.CharField(max_length=50)
+    premium_amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+
+class ActivationMetadataSerializer(serializers.Serializer):
+    policy_duration = serializers.DateField(write_only=True, input_formats=["%Y-%m-%d"])
+    renew = serializers.BooleanField()
+
+    def validate_policy_duration(self, value) -> Union[bool, ValidationError]:
+        """Ensures that a policy duration date is valid (not in the past)"""
+        today = datetime.now()
+
+        try:
+            policy_duration = datetime.strptime(value, "%Y-%m-%d")
+            if policy_duration <= today:
+                raise ValidationError("Policy duration must be a future date.")
+        except ValueError:
+            raise ValidationError("Invalid date format for policy duration")
+        return value
+
+
+class PolicyPurchaseSerializer(serializers.Serializer):
+    """
+    Validates the purchase request payload
+
+    Issues a new policy based on customer information and some
+    other metadata
+
+    For example:
+
+    ```json
+        {
+          "customer_information": {
+            "customer_name": "John Doe",
+            "customer_email": "john.doe@example.com",
+            "customer_phone": "123-456-7890",
+            "customer_address": "123 Main St, Anytown, Country",
+          },
+          'quote_code': 'quo_DADDY_HELP_ME',
+          "product_selection": {
+            "product_name": "Smart Motorist Protection Plan",
+            "product_type": "Basic",
+            "insurer": "NEM"
+          },
+          "payment_information": {
+            "payment_method": "credit_card",
+            'payment_status': 'completed',
+            "premium_amount": 2000.00,
+          },
+          "activation_details": {
+            "policy_duration": "1 year"
+            'renew': True # or False
+          },
+          "agreement": true,
+          "confirmation": true
+        }
+    ```
+    """
+
+    quote_code = serializers.CharField(required=True)
+    customer_metadata = CustomerDetailsSerializer()
+    product_metadata = ProductMetadataSerializer()
+    payment_metadata = PaymentInformationSerializer()
+    acivation_metadata = ActivationMetadataSerializer()
+    agreement = serializers.BooleanField(write_only=True)
+    confirmation = serializers.BooleanField(write_only=True)
+
+    def validate_agreement(self, value):
+        if not value:
+            raise ValidationError(
+                "Agreement must be confirmed to proceed with the purchase of this policy"
+            )
+        return value
+
+    def validate_confirmation(self, value):
+        if not value:
+            raise ValidationError(
+                "Confirmation must be provided to proceed with the purchase of this policy"
+            )
+        return value
