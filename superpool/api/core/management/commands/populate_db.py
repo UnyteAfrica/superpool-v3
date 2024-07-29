12c22d37f1efb156b41b0f4b3e9a2fc63b8fb99f
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-29 18:13:34 +0100

    chore(migrations): Update database table for Policy and Product Meta

--- superpool/api/core/management/commands/populate_db.py
+++ superpool/api/core/management/commands/populate_db.py
@@ -6,11 +6,11 @@ With this, we can have some data to work with when we start the application.
 Therefore, having actual testing experience
 """
 
-import multiprocessing
 import uuid
 from pprint import pprint
 
 from core.catalog.models import Policy, Product, Quote
+from core.management.fixtures.providers import InsuranceProductProvider
 from core.merchants.models import Merchant
 from core.models import Address, Coverage
 from core.providers.models import Provider as Partner
@@ -24,51 +24,53 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         fake = Faker()
+        insurance_provider = InsuranceProductProvider
+        fake.add_provider(insurance_provider)
 
         # Create 10 Merchants
-        for _ in range(10):
-            try:
-                Merchant.objects.create(
-                    name=fake.company(),
-                    business_email=fake.company_email(),
-                    support_email=fake.email(),
-                    tax_identification_number=fake.random_int(),
-                    registration_number=fake.random_int(),
-                    address=fake.address(),
-                    api_key=fake.uuid4(),
-                )
-            except Exception as e:
-                pprint(f"Error: {e}")
-                pass
+        # for _ in range(10):
+        # try:
+        #     Merchant.objects.create(
+        #         name=fake.company(),
+        #         business_email=fake.company_email(),
+        #         support_email=fake.email(),
+        #         tax_identification_number=fake.random_int(max=15),
+        #         registration_number=fake.random_int(max=15),
+        #         address=fake.address(),
+        #     )
+        # except Exception as e:
+        #     pprint(f"Error: {e}")
+        #     pass
 
         # Create 10 Insurance Providers
+        # for _ in range(10):
+        #     Partner.objects.create(
+        #         name=fake.company(),
+        #         support_email=fake.company_email(),
+        #         support_phone=fake.phone_number(),
+        #     )
+
+        # # Create 100 Customers
+        # for _ in range(100):
+        # try:
+        #     Customer.objects.create(
+        #         first_name=fake.first_name(),
+        #         middle_name=fake.middle_name(),
+        #         last_name=fake.last_name(),
+        #         gender=fake.random_element(elements=("M", "F")),
+        #         dob=fake.date_of_birth(),
+        #         email=fake.email(),
+        #         phone_number=fake.phone_number(),
+        #         address=fake.address(),
+        #         kyc_verified=fake.boolean(),
+        #     )
+        # except Exception as e:
+        #     pprint(f"Error: {e}")
+        #     pass
+
+        # # Create 5 Products of 6 ditinct insurance types
+
         for _ in range(10):
-            Partner.objects.create(
-                name=fake.company(),
-                support_email=fake.company_email(),
-                support_phone=fake.phone_number(),
-            )
-
-        # Create 100 Customers
-        for _ in range(100):
-            try:
-                Customer.objects.create(
-                    first_name=fake.first_name(),
-                    middle_name=fake.middle_name(),
-                    last_name=fake.last_name(),
-                    gender=fake.random_element(elements=("M", "F")),
-                    dob=fake.date_of_birth(),
-                    email=fake.email(),
-                    phone_number=fake.phone_number(),
-                    address=fake.address(),
-                    kyc_verified=fake.boolean(),
-                )
-            except Exception as e:
-                pprint(f"Error: {e}")
-                pass
-
-        # Create 5 Products of 6 ditinct insurance types
-        for _ in range(5):
             try:
                 Product.objects.create(
                     name=fake.random_company_product(),
@@ -76,8 +78,8 @@ class Command(BaseCommand):
                     product_type=fake.random_element(
                         elements=(
                             "Auto",
-                            "CreditLife",
-                            "Home",
+                            # "CreditLife",
+                            "Life",
                             "Travel",
                             "Health",
                             "Gadget",
@@ -89,81 +91,81 @@ class Command(BaseCommand):
                 pprint(f"Error: {e}")
                 pass
 
-        # Create 20 Policies
-        for _ in range(20):
-            try:
-                Policy.objects.create(
-                    policy_id=uuid.uuid4(),
-                    policy_number=fake.bothify(text="POL######"),
-                    product=Product.objects.order_by("?").first(),
-                    product_type=fake.random_element(
-                        elements=(
-                            "Auto",
-                            "CreditLife",
-                            "Home",
-                            "Travel",
-                            "Health",
-                            "Gadget",
-                        )
-                    ),
-                    customer=Customer.objects.order_by("?").first(),
-                    merchant=Merchant.objects.order_by("?").first(),
-                    address=Address.objects.order_by("?").first(),
-                    start_date=fake.date_this_decade(),
-                    end_date=fake.date_this_decade(end_date="+1y"),
-                    premium=fake.random_number(digits=5),
-                    status=fake.random_element(
-                        elements=("active", "cancelled", "expired")
-                    ),
-                )
-            except Exception as e:
-                pprint(f"Error: {e}")
-                pass
-
-        # Create 20 Quotess
-        for _ in range(20):
-            try:
-                Quote.objects.create(
-                    quote_code=fake.uuid4(),
-                    product=Product.objects.order_by("?").first(),
-                    customer=Customer.objects.order_by("?").first(),
-                    merchant=Merchant.objects.order_by("?").first(),
-                    price=fake.random_number(digits=5),
-                    status=fake.random_element(
-                        elements=("pending", "accepted", "rejected")
-                    ),
-                )
-            except Exception as e:
-                pprint(f"Error: {e}")
-                pass
-
-        # Create 20 Addresses
-        for _ in range(20):
-            try:
-                Address.objects.create(
-                    street=fake.street_address(),
-                    city=fake.city(),
-                    state=fake.state(),
-                    country=fake.country(),
-                )
-            except Exception as e:
-                pprint(f"Error: {e}")
-                pass
-
-        # Create 20 Coverages
-        for _ in range(20):
-            try:
-                Coverage.objects.create(
-                    coverage_name=fake.random_element(
-                        elements=("Liability", "Comprehensive", "Collision")
-                    ),
-                    coverage_id=uuid.uuid4(),
-                    coverage_limit=fake.random_number(digits=5),
-                    description=fake.sentence(),
-                    product_id=Product.objects.order_by("?").first(),
-                )
-            except Exception as e:
-                pprint(f"Error: {e}")
-                pass
+        # # Create 20 Policies
+        # for _ in range(20):
+        #     try:
+        #         Policy.objects.create(
+        #             policy_id=uuid.uuid4(),
+        #             policy_number=fake.bothify(text="POL######"),
+        #             product=Product.objects.order_by("?").first(),
+        #             product_type=fake.random_element(
+        #                 elements=(
+        #                     "Auto",
+        #                     "CreditLife",
+        #                     "Home",
+        #                     "Travel",
+        #                     "Health",
+        #                     "Gadget",
+        #                 )
+        #             ),
+        #             customer=Customer.objects.order_by("?").first(),
+        #             merchant=Merchant.objects.order_by("?").first(),
+        #             address=Address.objects.order_by("?").first(),
+        #             start_date=fake.date_this_decade(),
+        #             end_date=fake.date_this_decade(end_date="+1y"),
+        #             premium=fake.random_number(digits=5),
+        #             status=fake.random_element(
+        #                 elements=("active", "cancelled", "expired")
+        #             ),
+        #         )
+        #     except Exception as e:
+        #         pprint(f"Error: {e}")
+        #         pass
+        #
+        # # Create 20 Quotess
+        # for _ in range(20):
+        #     try:
+        #         Quote.objects.create(
+        #             quote_code=fake.uuid4(),
+        #             product=Product.objects.order_by("?").first(),
+        #             customer=Customer.objects.order_by("?").first(),
+        #             merchant=Merchant.objects.order_by("?").first(),
+        #             price=fake.random_number(digits=5),
+        #             status=fake.random_element(
+        #                 elements=("pending", "accepted", "rejected")
+        #             ),
+        #         )
+        #     except Exception as e:
+        #         pprint(f"Error: {e}")
+        #         pass
+        #
+        # # Create 20 Addresses
+        # for _ in range(20):
+        #     try:
+        #         Address.objects.create(
+        #             street=fake.street_address(),
+        #             city=fake.city(),
+        #             state=fake.state(),
+        #             country=fake.country(),
+        #         )
+        #     except Exception as e:
+        #         pprint(f"Error: {e}")
+        #         pass
+        #
+        # # Create 20 Coverages
+        # for _ in range(20):
+        #     try:
+        #         Coverage.objects.create(
+        #             coverage_name=fake.random_element(
+        #                 elements=("Liability", "Comprehensive", "Collision")
+        #             ),
+        #             coverage_id=uuid.uuid4(),
+        #             coverage_limit=fake.random_number(digits=5),
+        #             description=fake.sentence(),
+        #             product_id=Product.objects.order_by("?").first(),
+        #         )
+        #     except Exception as e:
+        #         pprint(f"Error: {e}")
+        #         pass
 
         pprint("Database populated successfully")
