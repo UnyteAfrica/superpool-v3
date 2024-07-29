656aa05dd86e289802006e901e9acae67880ebcc
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-29 17:52:12 +0100

    chore: add docs to fixture provider generator

--- superpool/api/core/management/fixtures/providers.py
+++ superpool/api/core/management/fixtures/providers.py
@@ -1,3 +1,17 @@
+"""
+This module serves as a patch to resolve a bug in the faker library.
+The bug is that the company provider does not have a method to generate a random company product.
+
+Root Cause Analysis: the `random_company_product` method provided by the Provider class is not implemented in the company provider.
+Nor does it provide a way to generate a random company product resulting in an Error from the `Generator`.
+
+Solution: I have created a custom provider called `InsuranceProductProvider` that inherits from the `Provider` class. It
+provides the concrete implementation of the `random_company_product` method to generate a random company product,
+and more specifically, tailored to our company need, an insurance product.
+
+Date created: 2024-07-29 16:00:00
+"""
+
 from faker import Faker
 from faker.providers.company import Provider
 
