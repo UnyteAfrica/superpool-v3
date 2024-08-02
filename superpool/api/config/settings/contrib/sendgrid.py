bc032211b4de82d2adbfa220ef8872a70e6d399b
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-02 15:52:49 +0100

    refactor(email provider): Update email provider to support mailgun as temporary provider

--- superpool/api/config/settings/contrib/sendgrid.py
+++ superpool/api/config/settings/contrib/sendgrid.py
@@ -4,21 +4,52 @@ from django.conf import settings
 
 from ..environment import env
 
-if settings.DEBUG:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-    EMAIL_HOST = "localhost"
-    EMAIL_PORT = env.int("EMAIL_PORT", default=1025)
-    EMAIL_USE_TLS = False
-    EMAIL_HOST_USER = ""
-    EMAIL_HOST_PASSWORD = ""
-    FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="webmaster@localhost.unyte.com")
+MAILGUN_ENABLED = env.bool("MAILGUN_ENABLED", default=False)
+SENDGRID_ENABLED = env.bool("SENDGRID_ENABLED", default=False)
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("EMAIL_HOST", default="smtp.sendgrid.net")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = env("SENDGRID_API_KEY")
+if MAILGUN_ENABLED and SENDGRID_ENABLED:
+    raise ValueError(
+        "Only one email provider can be enabled at a time. "
+        "Both Mailgun and Sendgrid are enabled. Please disable one."
+    )
 
-FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="tech@unyte.com")
-DEFAULT_FROM_EMAIL = FROM_EMAIL
+match (settings.DEBUG, MAILGUN_ENABLED, SENDGRID_ENABLED):
+    # If DEBUG is True, use the console backend
+    case (True, _, _):
+        EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+        EMAIL_HOST = "localhost"
+        EMAIL_PORT = 1025
+        EMAIL_USE_TLS = False
+        EMAIL_HOST_USER = ""
+        EMAIL_HOST_PASSWORD = ""
+        FROM_EMAIL = env("FROM_EMAIL", default="webmaster@localhost")
+        DEFAULT_FROM_EMAIL = FROM_EMAIL
+
+    # If Mailgun is enabled, use the Mailgun backend
+    case (False, True, _):
+        EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+        EMAIL_HOST = env("MAILGUN_SMTP_SERVER")
+        EMAIL_PORT = env("MAILGUN_SMTP_PORT")
+        EMAIL_PORT = 587
+        EMAIL_USE_TLS = True
+        EMAIL_HOST_USER = env("MAILGUN_EMAIL_USERNAME")
+        EMAIL_HOST_PASSWORD = env("MAILGUN_EMAIL_PASSWORD")
+        SUPERPOOL_NS_EMAIL = env("SUPERPOOL_NOTIFICATION_SERVICE_FROM_EMAIL")
+        DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+
+    # If Sendgrid is enabled, use Sendgrid backend
+    case (False, _, True):
+        EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+        EMAIL_HOST = env("EMAIL_HOST", default="tech@unyte.africa")
+        EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+        EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+        EMAIL_USE_TLS = True
+        EMAIL_PORT = env("EMAIL_PORT")
+        FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+        DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+
+    # You fucked up!
+    case _:
+        raise ValueError(
+            "No email provider was configured. Please configure either Sendgird or Mailgun"
+        )
