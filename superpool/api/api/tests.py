bd27210b54d49f6d0f7ef81cbb2366d3c0f3e921
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-05 09:26:06 +0100

    test(tenant-middleware): setup test signatures

--- superpool/api/api/tests.py
+++ superpool/api/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
 
-# Create your tests here.
+# WE WOULD BE TESTING OUR MIDDLEWARE SINCE ITS MEANT TO INTERACT WITH
+# THE REQUEST AND RESPONSE OBJECTS
+
+
+def test_request_authenticated_with_middleware_and_correct_credentials():
+    pass
+
+
+def test_request_unauthenticated_with_middleware_and_incorrect_credentials():
+    pass
+
+
+def test_request_unauthenticated_with_middleware_and_no_credentials():
+    pass
