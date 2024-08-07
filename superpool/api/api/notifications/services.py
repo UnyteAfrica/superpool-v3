fa61611a95efd61e20b46dfc72f6fa37aa5aefd0
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-07 22:03:52 +0100

    feat: update policy notification logic for merchants

--- superpool/api/api/notifications/services.py
+++ superpool/api/api/notifications/services.py
@@ -17,7 +17,17 @@ class PolicyNotificationService(NotificationService):
     @staticmethod
     def notify_merchant(action: str, policy: "Policy") -> None:
         """Send a notification to the merchant about the action that took place on the policy"""
+
         print(f"Sending notification to merchant for action: {action}")
+        service = NotificationService()
+        message_data = service.prepare_message(action, "merchant")
+        service.stream_through(NotificationService.EMAIL)
+        service.send(
+            recipient=policy.merchant.business_email,
+            subject=message_data["subject"],
+            message=message_data["body"],
+        )
+        logger.info(f"Notification sent to {policy.merchant.business_email}")
 
     @staticmethod
     def notify_customer(
@@ -33,7 +43,7 @@ class PolicyNotificationService(NotificationService):
         # We would assume the policy holder is to be notified if the user information
         # is not passed to this function
         recipient = customer_email or policy.policy_holder.email
-        message_data = service.prepare_message(action, recipient)
+        message_data = service.prepare_message(action, "customer")
         print(f"Message data: {message_data}")
 
         _channel = extra_kwargs.get("channel", NotificationService.EMAIL)
