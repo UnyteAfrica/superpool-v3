d92c2321fae90050aeadb6dba6756abb0359a7c0
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-07 22:00:51 +0100

    feat: Update Policy Notificaiton logic for customer

--- superpool/api/api/notifications/services.py
+++ superpool/api/api/notifications/services.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Any, Dict, Union
 
 from core.catalog.models import Policy
 
 from .base import NotificationService
+
+logger = logging.getLogger(__name__)
 
 
 class PolicyNotificationService(NotificationService):
@@ -18,8 +21,26 @@ class PolicyNotificationService(NotificationService):
 
     @staticmethod
     def notify_customer(
-        action: str, policy: "Policy", **extra_kwargs: Dict[str, Any]
+        action: str,
+        policy: "Policy",
+        customer_email: Union[str, None] = None,
+        **extra_kwargs: Dict[str, Any],
     ) -> None:
         """Send a notification to the customer about the action that took place on the policy"""
 
         print(f"Sending notification to customer for action: {action}")
+        service = NotificationService()
+        # We would assume the policy holder is to be notified if the user information
+        # is not passed to this function
+        recipient = customer_email or policy.policy_holder.email
+        message_data = service.prepare_message(action, recipient)
+        print(f"Message data: {message_data}")
+
+        _channel = extra_kwargs.get("channel", NotificationService.EMAIL)
+        service.stream_through(_channel)
+        service.send(
+            recipient=recipient,
+            subject=message_data["subject"],
+            message=message_data["body"],
+        )
+        logger.info(f"Notification sent to {recipient} via {_channel}")
