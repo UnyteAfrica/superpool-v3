f01f4f1a0e4d1366fe58fc820894567255bd50de
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-07 18:18:40 +0100

    refactor(notifications): application not sending policy purchase email to stakeholders

--- superpool/api/api/notifications/channels.py
+++ superpool/api/api/notifications/channels.py
@@ -9,46 +9,14 @@ class EmailNotification(INotification):
     Email Notification service
     """
 
-    @classmethod
-    def notify(cls, who, action, policy):
-        pass
-
-    @staticmethod
-    def send_message(subject: str, message: str, recipient: str) -> None:
-        """
-        Sends a message to a recipient via Email
-        """
-        send_mail(
-            subject,
-            message,
-            settings.DEFAULT_FROM_EMAIL,
-            [recipient],
-        )
-
 
 class SMSNotification(INotification):
     """
     SMS Notification service
     """
 
-    @staticmethod
-    def send_message(subject: str, message: str, recipient: str) -> None:
-        """
-        Sends a message to a recipient via SMS
-        """
-        # TODO: In future, we can implement the SMS service
-        pass
-
 
 class WhatsAppNotification(INotification):
     """
     WhatsApp Notification service
     """
-
-    @staticmethod
-    def send_message(message: str, recipient: str) -> None:
-        """
-        Sends a message to a recipient via Whatsapp
-        """
-        # TODO: In future, we can implement the WhatsApp APIs
-        pass
