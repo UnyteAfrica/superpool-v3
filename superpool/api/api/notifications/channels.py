525d3667155f1bde7d48179d657f898713062c1b
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-07 19:04:26 +0100

    refactor: major refactoring to notification channels

--- superpool/api/api/notifications/channels.py
+++ superpool/api/api/notifications/channels.py
@@ -9,11 +9,29 @@ class EmailNotification(INotification):
     Email Notification service
     """
 
+    def send(self, recipient: str, subject: str, message: str) -> None:
+        """
+        Send the email message to the recipient
+        """
+        print(
+            f"Sending email to {recipient} with subject: {subject} and message: {message}"
+        )
+        pass
+
 
 class SMSNotification(INotification):
     """
     SMS Notification service
     """
+
+    def send(self, recipient: str, subject: str, message: str) -> None:
+        """
+        Send the SMS message to the recipient
+        """
+        print(
+            f"Sending SMS to {recipient} with subject: {subject} and message: {message}"
+        )
+        pass
 
 
 class WhatsAppNotification(INotification):
