1acd1edcc3cce82c4dce3d6d430cb9ec22edc114
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-09 10:31:45 +0100

    Implement CLI arguments for purchasing single policy

--- superpool/api/core/management/commands/purchase_policy.py
+++ superpool/api/core/management/commands/purchase_policy.py
@@ -4,7 +4,7 @@ from django.utils.dateparse import parse_date
 import sys
 from decimal import Decimal
 
-from core.catalog.models import Quote
+from core.catalog.models import Quote, Policy
 from core.merchants.models import Merchant
 from api.catalog.services import PolicyService
 
@@ -14,84 +14,89 @@ class Command(BaseCommand):
     This command is used to create a purchase insurance policy for a given quote
     """
 
-    help = "urchase a policy by providing necessary details"
+    help = "Purchase a policy by providing necessary details"
 
     def add_arguments(self, parser) -> None:
         parser.add_argument(
-            "quote_code",
-            type=str,
-            help="Quote code for which policy needs to be purchased",
+            "quote_code", type=str, help="Quote code to purchase policy for"
+        )
+        parser.add_argument(
+            "--first_name", type=str, help="First name of the policy holder"
+        )
+        parser.add_argument(
+            "--last_name", type=str, help="Last name of the policy holder"
         )
         parser.add_argument("customer_email", type=str, help="Email of the customer")
         parser.add_argument(
-            "product_name", type=str, help="Name of the insurance policy"
+            "--customer_phone", type=str, help="Phone number of the customer"
         )
         parser.add_argument(
-            "product_type", type=str, help="Type of the insurance policy"
+            "--customer_date_of_birth",
+            type=str,
+            help="Date of birth of the policy holder",
         )
-        parser.add_argument("payment_method", type=str)
-        parser.add_argument("payment_status", type=str)
-        parser.add_argument("premium_amount", type=Decimal)
-        parser.add_argument("policy_expiry_date", type=str)
         parser.add_argument(
-            "renew", type=bool, default=False, help="Should the policy be renewed?"
+            "--customer_gender",
+            choices=["M", "F"],
+            type=str,
+            help="Sexual orientation of the policy holder",
         )
-        parser.add_argument("agreement", type=bool, default=True)
-        parser.add_argument("confirmation", type=bool, default=True)
+        parser.add_argument(
+            "policy_name",
+            type=str,
+            help="Name of the insurance policy (or product) to purchase",
+        )
+        parser.add_argument(
+            "product_type",
+            type=str,
+            help="Type of the insurance product or policy",
+        )
+        parser.add_argument(
+            "product_code",
+            type=str,
+            help="Code of the insurance product or policy",
+            required=False,
+        )
+        parser.add_argument(
+            "payment_method",
+            type=str,
+            help="Payment method to use for the purchase",
+            choices=["card", "wallet", "bank_transfer", "online_banking"],
+        )
+        parser.add_argument(
+            "payment_reference",
+            type=str,
+            help="Reference number of the payment made",
+            required=False,
+        )
+        parser.add_argument(
+            "payment_status",
+            type=str,
+            choices=["completed", "pending"],
+            help="Status of the payment",
+            optional=True,
+        )
+        parser.add_argument(
+            "premium_amount",
+            type=Decimal,
+            help="Amount paid for the policy",
+            required=False,
+        )
+        parser.add_argument(
+            "policy_expiry_date",
+            type=str,
+            help="Date the policy is expected to expire in the format YYYY-MM-DD",
+        )
+        parser.add_argument(
+            "--renew",
+            action="store_true",
+            help="Flag to indicate if the policy should be renewed",
+        )
         parser.add_argument(
             "merchant_code",
             type=str,
             help="Unique short code identifier assigned to the merchant",
         )
 
-    def handle(self, *args, **options):
-        try:
-            quote_code = options["quote_code"]
-            customer_email = options["customer_email"]
-            product_name = options["product_name"]
-            product_type = options["product_type"]
-            payment_method = options["payment_method"]
-            payment_status = options["payment_status"]
-            premium_amount = options["premium_amount"]
-            policy_expiry_date = parse_date(options["policy_expiry_date"])
-            renew = options["renew"]
-            agreement = options["agreement"]
-            confirmation = options["confirmation"]
-            merchant_code = options["merchant_code"]
-
-            quote = Quote.objects.get(quote_code=quote_code)
-
-            # TODO: fetch neccessary information from quotes
-
-            policy_data = {
-                "quote_code": quote_code,
-                "customer_metadata": {
-                    "customer_email": customer_email,
-                },
-                "product_metadata": {
-                    "product_name": product_name,
-                    "product_type": product_type,
-                },
-                "payment_metadata": {
-                    "payment_method": payment_method,
-                    "payment_status": payment_status,
-                    "premium_amount": premium_amount,
-                },
-                "activation_metadata": {
-                    "policy_expiry_date": policy_expiry_date,
-                    "renew": renew,
-                },
-                "agreement": agreement,
-                "confirmation": confirmation,
-                "merchant_code": merchant_code,
-            }
-
-            PolicyService.purchase_policy(policy_data)
-        except ValidationError as e:
-            sys.stderr.write(f"Validation Error: {e}")
-        except Quote.DoesNotExist:
-            sys.stderr.write("Quote does not exist")
-        except Merchant.DoesNotExist:
-            sys.stderr.write("Merchant does not exist")
-        except Exception as e:
-            sys.stderr.write(f"Error: {e}")
+    def handle(self, args, options) -> "Policy" | None:
+        pass
