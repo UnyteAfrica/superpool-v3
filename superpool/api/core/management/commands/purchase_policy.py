334e0490870fc6cbdc64ffd2ae5d473ae6b66283
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-09 11:13:51 +0100

    fix(purchas_policy): change all arguments to optional arguments
    An error resulted from setting `required=False` in the postional
    arguments, which were not supported. As a solution, an approach is to
    convert all postional argument to optional arguments, which helps with
    readability and maintainability as well.
    
    This patch did just that!

--- superpool/api/core/management/commands/purchase_policy.py
+++ superpool/api/core/management/commands/purchase_policy.py
@@ -20,92 +20,99 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser) -> None:
         parser.add_argument(
-            "quote_code", type=str, help="Quote code to purchase policy for"
+            "--quote_code",
+            type=str,
+            required=True,
+            help="Quote code to purchase policy for",
         )
         parser.add_argument(
             "--first_name",
             type=str,
-            help="First name of the policy holder",
             required=True,
+            help="First name of the policy holder",
         )
         parser.add_argument(
             "--last_name",
             type=str,
-            help="Last name of the policy holder",
             required=True,
+            help="Last name of the policy holder",
         )
         parser.add_argument(
-            "customer_email", type=str, help="Email of the customer", required=True
+            "--customer_email", type=str, required=True, help="Email of the customer"
         )
         parser.add_argument(
             "--customer_phone",
             type=str,
-            help="Phone number of the customer",
             required=True,
+            help="Phone number of the customer",
         )
         parser.add_argument(
             "--customer_address",
             type=str,
-            help="Address of the policy holder",
             required=False,
+            help="Address of the policy holder",
         )
         parser.add_argument(
             "--customer_date_of_birth",
             type=str,
-            help="Date of birth of the policy holder",
             required=False,
+            help="Date of birth of the policy holder",
         )
         parser.add_argument(
             "--customer_gender",
             choices=["M", "F"],
             type=str,
+            required=False,
             help="Sexual orientation of the policy holder",
         )
         parser.add_argument(
-            "policy_name",
+            "--policy_name",
             type=str,
+            required=True,
             help="Name of the insurance policy (or product) to purchase",
         )
         parser.add_argument(
-            "product_type",
+            "--product_type",
             type=str,
+            required=True,
             help="Type of the insurance product or policy",
         )
         parser.add_argument(
-            "product_code",
+            "--product_code",
             type=str,
-            help="Code of the insurance product or policy",
             required=False,
+            help="Code of the insurance product or policy",
         )
         parser.add_argument(
-            "payment_method",
+            "--payment_method",
             type=str,
-            help="Payment method to use for the purchase",
             choices=["card", "wallet", "bank_transfer", "online_banking"],
             default="card",
-            required=False,
+            help="Payment method to use for the purchase",
         )
         parser.add_argument(
-            "payment_reference",
+            "--payment_reference",
             type=str,
-            help="Reference number of the payment made",
             required=False,
+            help="Reference number of the payment made",
         )
         parser.add_argument(
-            "payment_status",
+            "--payment_status",
             type=str,
             choices=["completed", "pending"],
             help="Status of the payment",
-            optional=True,
+            required=False,
         )
         parser.add_argument(
-            "premium_amount",
+            "--premium_amount",
             type=Decimal,
+            required=True,
             help="Amount paid for the policy",
         )
         parser.add_argument(
-            "policy_expiry_date",
+            "--policy_expiry_date",
             type=str,
+            required=True,
             help="Date the policy is expected to expire in the format YYYY-MM-DD",
         )
         parser.add_argument(
@@ -114,12 +121,13 @@ class Command(BaseCommand):
             help="Flag to indicate if the policy should be renewed",
         )
         parser.add_argument(
-            "merchant_code",
+            "--merchant_code",
             type=str,
+            required=True,
             help="Unique short code identifier assigned to the merchant",
         )
 
-    def handle(self, args, options) -> "Policy" | None:
+    def handle(self, args, options):
         try:
             policy_data = {
                 "quote_code": options["quote_code"],
@@ -145,7 +153,7 @@ class Command(BaseCommand):
 
             policy = PolicyService.purchase_policy(policy_data)
             sys.stdout.write(
-                f"Policy purchased successfully for quote {options['quote_code']}: {policy.id} \n"
+                f"Policy purchased successfully for quote {options['quote_code']}: {policy.policy_id} \n"
             )
             sys.stdout.write(f"Policy details: {policy}\n")
         except ValueError as e:
