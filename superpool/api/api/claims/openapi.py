2253e67ee7e8382f3daa85055d2f0db003d72ee7
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-13 22:20:26 +0100

    feat: add openapi response example for single claim

--- superpool/api/api/claims/openapi.py
+++ superpool/api/api/claims/openapi.py
@@ -86,3 +86,29 @@ full_claim_request_payload_example = OpenApiExample(
         },
     },
 )
+
+single_claim_response_example = OpenApiExample(
+    "Claim informaton",
+    value={
+        "claim_reference_number": "CLAIM-20240813-5678",
+        "claim_status": "approved",
+        "claim_date": "2024-08-13",
+        "customer": {
+            "first_name": "Michael",
+            "last_name": "Smith",
+            "dob": "1975-04-22",
+            "email": "michaelsmith@example.com",
+            "phone_number": "+9876543210",
+        },
+        "claim_amount": "3200.00",
+        "insurer": "Leadway Assurance",
+        "product": {"name": "Auto Insurance", "product_type": "Automobile"},
+        "policy": "dc4bf271-6491-3812-8f39-58d4ccf39b70",
+        "claim_status_timeline": [
+            {"time_stamp": "2024-08-13 08:45:00", "name": "submitted"},
+            {"time_stamp": "2024-08-13 10:00:00", "name": "under review"},
+            {"time_stamp": "2024-08-13 12:15:00", "name": "pending approval"},
+            {"time_stamp": "2024-08-13 14:30:00", "name": "approved"},
+        ],
+    },
+)
