b400b1c7d5050d0afa3a8200081ac071d574a322
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-13 07:55:18 +0100

    feat(claims): improve the claim schema, and implement new claim evidence table

--- superpool/api/core/claims/models.py
+++ superpool/api/core/claims/models.py
@@ -1,7 +1,7 @@
 import uuid
 
 from core.catalog.models import Policy, Product
-from core.mixins import TimestampMixin
+from core.mixins import TimestampMixin, TrashableModelMixin
 from core.providers.models import Provider
 from core.user.models import Customer
 from django.db import models
@@ -47,11 +47,20 @@ class Claim(TimestampMixin, models.Model):
         verbose_name=_("Date at which a claim is created"),
     )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    estimated_loss = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    payout_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     policy = models.ForeignKey(Policy, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     status = models.CharField(max_length=30, choices=CLAIM_STATUS, default="pending")
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    notes = models.TextField(null=True, blank=True)
+    documents = models.ManyToManyField(
+        "ClaimDocument",
+        related_name="claims",
+        verbose_name=_("Documents"),
+        help_text=_("Documents uploaded by the policyholder as part of the claim"),
+    )
 
     class Meta:
         verbose_name = "claim"
@@ -90,3 +99,80 @@ class StatusTimeline(models.Model):
     )
     status = models.CharField(max_length=30, choices=Claim.CLAIM_STATUS)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class ClaimDocument(models.Model):
+    """
+    Represents a document uploaded by a policyholder as part of a claim.
+    """
+
+    VIDEO = "video"
+    AUDIO = "audio"
+    DOCUMENT = "document"
+    PICTURE = "picture"
+
+    EVIDENCE_TYPE_CHOICES = [
+        (VIDEO, _("Video")),
+        (AUDIO, _("Audio")),
+        (DOCUMENT, _("Document")),
+        (PICTURE, _("Picture")),
+    ]
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    claim = models.ForeignKey(
+        "Claim",
+        related_name="documents",
+        on_delete=models.CASCADE,
+        verbose_name=_("Related Claim"),
+    )
+    evidence_type = models.CharField(
+        choices=EVIDENCE_TYPE_CHOICES,
+        max_length=10,
+        verbose_name=_("Evidence Type"),
+        help_text=_("Type of evidence uploaded."),
+    )
+    document_name = models.CharField(
+        max_length=255,
+        verbose_name=_("Document Name"),
+        help_text=_("Name of the document uploaded."),
+    )
+    blob = models.CharField(
+        max_length=1024,
+        verbose_name=_("File Reference"),
+        help_text=_(
+            "Reference to the stored document (e.g., Google Cloud Storage blob name)."
+        ),
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Claim Document")
+        verbose_name_plural = _("Claim Documents")
+        ordering = ["-uploaded_at"]
+
+    def __str__(self) -> str:
+        return f"{self.document_name} for claim #{self.claim.claim_number} if {self.claim.claim_number} else {self.claim.id}"
+
+    @property
+    def download_url(self):
+        """
+        Returns a URL to download the document.
+        """
+        # TODO: Implement this!
+        # should return the actual URL (public endpoint from gcp bucket to download the document)
+        # e.g f"https://example.com/download/{self.blob}"
+        pass
+
+    @property
+    def preview_url(self):
+        """
+        Returns a URL to preview the document.
+        """
+        # TODO: Implement this!
+        # should be replaced with the actual URL
+        # and be in the form of f"https://example.com/preview/{self.blob}"
+        pass
