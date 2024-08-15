df3e1825a4aa8890d1d024aa1155b3b15e8b6868
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-15 14:13:09 +0100

    feat: add browsabl API renderer to rest framework settings

--- superpool/api/config/settings/contrib/rest_framework.py
+++ superpool/api/config/settings/contrib/rest_framework.py
@@ -28,5 +28,6 @@ else:
         "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
         "DEFAULT_RENDERER_CLASSES": [
             "rest_framework.renderers.JSONRenderer",
+            "rest_framework.renderers.BrowsableAPIRenderer",
         ],
     }
