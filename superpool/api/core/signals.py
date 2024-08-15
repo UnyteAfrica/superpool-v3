082d6fdf75637c5fa022274a28094f3fd7babca7
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-15 17:04:38 +0100

    refactor: refactor signals to use transactions

--- superpool/api/core/signals.py
+++ superpool/api/core/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from core.permissions import assign_user_to_group
 from core.user.models import Admin, CustomerSupport
 from core.merchants.models import Merchant
+from django.db import transaction
 
 
 @receiver(post_save, sender=Admin)
@@ -11,8 +12,11 @@ def assign_admin_group(sender, instance, created, **kwargs):
     Assigns the user to the Admin group
     """
     if created:
-        assign_user_to_group(instance.user, "Admin")
-        instance.save()
+
+        def commit():
+            assign_user_to_group(instance.user, "Admin")
+
+        transaction.on_commit(commit)
 
 
 @receiver(post_save, sender=CustomerSupport)
@@ -21,8 +25,11 @@ def assign_support_group(sender, instance, created, **kwargs):
     Assigns the user to the Customer Support group
     """
     if created:
-        assign_user_to_group(instance.user, "CustomerSupport")
-        instance.save()
+
+        def commit():
+            assign_user_to_group(instance.user, "CustomerSupport")
+
+        transaction.on_commit(commit)
 
 
 @receiver(post_save, sender=Merchant)
@@ -31,5 +38,8 @@ def assign_merchant_group(sender, instance, created, **kwargs):
     Assign created user to the Merchant group
     """
     if created:
-        assign_user_to_group(instance.user, "Merchant")
-        instance.save()
+
+        def commit():
+            assign_user_to_group(instance.user, "Merchant")
+
+        transaction.on_commit(commit)
