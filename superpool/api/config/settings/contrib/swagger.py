e2c342ee56a8eeafa5f4c57b1389c1405f212e22
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-08-15 17:02:30 +0100

    docs: add authentication schemes to openapi

--- superpool/api/config/settings/contrib/swagger.py
+++ superpool/api/config/settings/contrib/swagger.py
@@ -1,14 +1,32 @@
 AUTHOR = "Unyte Africa LTD."
 
+API_VERSION = "1.4.0"
+
 # TODO: Dynamically insert the correct fields
 SPECTACULAR_SETTINGS = {
     "TITLE": "Superpool API",
     "DESCRIPTION": "API for Superpool",
-    "VERSION": "0.1.0",
+    "VERSION": API_VERSION,
     "AUTHOR": AUTHOR,
     "CONTACT": {
         "name": AUTHOR,
         "url": "https://ng.unyte.africa",
         "email": "tech@unyte.com",
     },
+    "AUTHENTICATION": [
+        {
+            "name": "APIKey",
+            "type": "apiKey",
+            "in": "header",
+            "x": {
+                "name": "Authorization",
+                "description": "API Key should be provided in the format `SUPERPOOL <key>`",
+            },
+        },
+        {"name": "Bearer", "type": "http", "scheme": "bearer", "bearerFormat": "JWT"},
+    ],
+    "SECURITY": [
+        {"APIKey": []},
+        {"Bearer": []},
+    ],
 }
