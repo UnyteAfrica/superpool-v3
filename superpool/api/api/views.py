8edf1857934a3314798b62c568530a8f82f15a70
Eri A. <sinmiwaye2001@gmail.com>
2024-08-28 02:17:46 +0100

    Revert "hotfix/fix cannot view insurance partner details"

--- superpool/api/api/views.py
+++ superpool/api/api/views.py
@@ -179,12 +179,14 @@ class InsuranceProviderDetailView(generics.RetrieveAPIView):
     # permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
-    lookup_url_kwarg = "name"
+    lookup_field = "id"
+    lookup_url_kwarg = "provider_id"
 
     def get(self, request, *args, **kwargs):
         """
-        Retrieve the details of a specific insurance provider.
+        Retrieve the details of a specific insurance provider
         """
+        return super().get(request, *args, **kwargs)
 
         provider_name = kwargs.get(self.lookup_url_kwarg)
 
