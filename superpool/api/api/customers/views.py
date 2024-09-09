98fac1dc6385f714f18352623e359ba0fd59e613
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-09-09 09:21:21 +0100

    chore: include imports for customers' serializers

---
+++ superpool/api/api/customers/views.py
@@ -0,0 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+from .serializers import CustomerInformationSerializer, CustomerSummarySerializer
