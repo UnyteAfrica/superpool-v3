2b355a41a111e9fb721886094f4e053402b8f053
50-Course <eridotdev@gmail.com>
2024-09-10 07:18:04 +0100

    feat: improve customer filtering with ability to filter by active claims

--- superpool/api/api/customers/filters.py
+++ superpool/api/api/customers/filters.py
@@ -4,6 +4,8 @@ from django.db.models import Q
 from django_filters import rest_framework as filters
 from rest_framework.filters import BaseFilterBackend
 from core.user.models import Customer
+from core.catalog.models import Policy
+from core.claims.models import Claim
 
 
 class CustomerFilter(filters.FilterSet):
@@ -11,12 +13,12 @@ class CustomerFilter(filters.FilterSet):
     Defines set of filters that can be applied onto a customer query lookup
     """
 
-    # full_name = filters.CharFilter(lookup_expr="icontains")
     first_name = filters.CharFilter(lookup_expr="iexact")
     last_name = filters.CharFilter(lookup_expr="iexact")
     email = filters.CharFilter(lookup_expr="icontains")
     has_active_policies = filters.BooleanFilter(method="filter_active_policies")
-    # has_pending_claims = filters.BooleanFilter(method="filter_pending_claims")
+    has_pending_claims = filters.BooleanFilter(method="filter_pending_claims")
+    has_active_claims = filters.BooleanFilter(method="filter_active_claims")
 
     class Meta:
         model = Customer
@@ -26,4 +28,19 @@ class CustomerFilter(filters.FilterSet):
         """
         Filters customers by those with active policies
         """
-        return qs.filter(policies__status="active")
+        return qs.filter(policies__status=Policy.ACTIVE).distinct()
+
+    def filter_active_claims(self, qs: QuerySet, name: str, value: Any) -> QuerySet:
+        """
+        Filter customers by those with active claims, that is:
+
+        anything that is not 'paid', 'denied', or 'pending'
+        """
+        return qs.exclude(
+            Q(claims__status="paid")
+            | Q(claims__status="denied")
+            | Q(claims__status="pending")
+        )
+
+    def filter_pending_claims(self, qs: QuerySet, name: str, value: Any) -> QuerySet:
+        return qs.filter(claims__status="pending")
