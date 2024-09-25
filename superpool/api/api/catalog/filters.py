682f9ecd88d5bc6537f0a4b3ea90ade4a385ed4e
50-Course <eridotdev@gmail.com>
2024-09-25 13:57:32 +0100

    feat: implement filtering class for quotes

--- superpool/api/api/catalog/filters.py
+++ superpool/api/api/catalog/filters.py
@@ -1,6 +1,8 @@
 from django_filters import rest_framework as filters
 from rest_framework.filters import SearchFilter
 
+from core.catalog.models import Quote
+
 
 class QSearchFilter(SearchFilter):
     search_param = "q"
@@ -12,12 +14,27 @@ class QuoteFilter(SearchFilter):
     and sorting by cheapest or best coverage.
     """
 
-    provider_name = filters.CharFilter(label="Insurer", lookup_expr="icontains")
+    provider_name = filters.CharFilter(
+        label="Provider Name",
+        lookup_expr="icontains",
+        field_name="product__provider__name",
+    )
     coverage_type = filters.CharFilter(
         method="filter_by_coverage_type", label="Coverage Type"
     )
+    # sort_by = filters.CharFilter(
+    #     method="sort_by_cheapest_or_best_coverage",
+    #     label="Sort by cheapest or best coverage limit",
+    # )
+    # we want to filter by prices greater than min_price and lesser than our max_price
+    min_price = filters.NumberFilter(field_name="premium__amount", lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="premium__amount", lookup_expr="lte")
 
-    sort_by = filters.CharFilter(
-        method="sort_by_cheapest_or_best_coverage",
-        label="Sort by cheapest or best coverage limit",
-    )
+    class Meta:
+        model = Quote
+        fields = ["provider_name", "coverage_type", "min_price", "max_price"]
+
+    def filter_by_coverage_type(self, queryset, name, value):
+        return queryset.filter(
+            additional_metadata__coverage_details__coverage_type__icontains=value
+        )
