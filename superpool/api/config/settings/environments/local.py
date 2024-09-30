0f0c0120676b642112194a4542b83d6d6eb57f14
50-Course <eridotdev@gmail.com>
2024-09-30 17:17:23 +0100

    Added Django Debug Toolbar and Django Silk to the development environment for performance monitoring and debugging.
    Why these packages:
    - Django Debug Toolbar helps inspect SQL queries, cache usage, and request/response timings, providing valuable insights during development.
    - Django Silk allows for detailed profiling, including SQL query performance, request/response cycle analysis, and memory usage tracking.
    - These tools will help identify bottlenecks and optimize performance during development before deploying to production.
    
    These packages are added as dev dependencies to avoid any unnecessary overhead or security risks in the production environment.
    They can be temporarily enabled in production if needed for detailed profiling in a controlled manner.
    
    Usage:
    - In order, to effectively profile in development, you have to switch
      your environment to `config.settings.environments.local` this is an
      extension of the current, `config.settings.base` which holds global
      configuratin for the entire project.
    - With this approach, we can seperate development monitoring from
      production which would be utilizing tracing and proper system
      monitoring tools
    - That said, you can easily switch your environments by doing this in
      your terminal:
    
      ```sh
      export DJANGO_SETTINGS_MODULE=config.settings.environments.local
      ```
    
    Changes:
    - Added django-debug-toolbar and django-silk to [tool.poetry.dev-dependencies].
    - Updated settings to enable these tools conditionally based on environment variables.
    - Updated environment template to include flag for turning on this
      profiling

--- superpool/api/config/settings/environments/local.py
+++ superpool/api/config/settings/environments/local.py
@@ -1,14 +1,49 @@
 try:
-    from core.settings.base import *  # noqa: F403
+    from config.settings.base import *  # noqa: F403
 except ImportError:
+    from config.settings.base import INSTALLED_APPS, MIDDLEWARE, env
+else:
     pass
 
-DEBUG = True
+# DEBUG = True
 
 TESTS = True
 
-DATABASES = {}
+# DATABASES = {}
 
-STORAGES = {}
+# STORAGES = {}
 
-CACHES = {}
+# CACHES = {}
+
+ENABLE_PROFILING = env.bool("ENABLE_PROFILING", default=False)
+
+if ENABLE_PROFILING:
+    INSTALLED_APPS += [
+        "debug_toolbar",
+        "silk",
+    ]
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+        "silk.middleware.SilkyMiddleware",
+    ]
+
+    # DJANGO DEBUG TOOLBAR CONFIGURATION
+    #
+    # Just some custom configuration to improve the efficency of our application
+    #
+    # See: https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html
+    # for more information.
+    DEBUG_TOOLBAR_CONFIG = {
+        "IS_RUNNING_TESTS": False,
+        # allow only admins access to debug and profiling panels
+        #
+        # this same thing is, configured below for `Silk` as boolen  flags
+        "SHOW_TOOLBAR_CALLBACK": lambda request: request.user.is_superuser,
+    }
+
+    # By default, silk allows dashboard access to anyone
+    #
+    # We want to limit that to only, authorized superusers
+    # Seee: https://pypi.org/project/django-silk/#Configuration
+    SILK_AUTHENTICATION = True
+    SILK_AUTHORIZATION = True
