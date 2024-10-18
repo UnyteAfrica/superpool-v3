787e6d3fc091aaa8e8386658f689f965d03c197a
Mbaoma <mmaryraphaella@gmail.com>
2024-10-18 17:02:09 +0100

    update cloudbuild command

--- superpool/api/celery_app.py
+++ superpool/api/celery_app.py
@@ -4,7 +4,10 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery("superpool", broker=os.getenv("CELERY_BROKER_URL", default="redis://redis:6379/0"), backend=os.getenv("CELERY_RESULT_BACKEND", default="redis://redis:6379/0"))
+app = Celery("superpool",
+    broker=os.getenv("CELERY_BROKER_URL", default="redis://redis:6379/0"),
+    backend=os.getenv("CELERY_RESULT_BACKEND", default="redis://redis:6379/0"),
+)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
